bf03c4a732ec46b2c710f8adcfaf9e150754000a
Twin <hi@twin.uk.com>
2017-02-15 18:03:45 +0000

    Finalised lab 1-3 code

--- code/lab03/mnist_deploy.py
+++ code/lab03/mnist_deploy.py
@@ -1,19 +1,15 @@
 import sys
 import numpy as np
 import caffe
+import time
 
 
 if len(sys.argv) < 1:
     pritn("No path to image, use as follows: \n mnist_deply.py path/to/image.jpg")
     sys.exit()
 
-
-
-caffe.set_mode_gpu()
-caffe.set_device(0)
-
-model_path = "code/practical_3/mnist_lenet_deploy.prototxt"
-weights_path = "code/practical_2/mnist_lenet_iter_10000.caffemodel"
+model_path = "code/lab03/mnist_lenet_deploy.prototxt"
+weights_path = "code/lab02/mnist_lenet_iter_10000.caffemodel"
 #Net loading parameters changed in Python 3
 net = caffe.Net(model_path, 1, weights=weights_path)
 
@@ -28,19 +24,47 @@ transformed_image = transformer.preprocess('data', image)
 # copy the image data into the memory allocated for the net
 net.blobs['data'].data[...] = transformed_image
 
+
+
+#Use CPU for training
+caffe.set_mode_cpu()
+
+cpuStartTime = time.time()
+
 ### perform classification
 output = net.forward()
+
+cpuEndTime = time.time()
+
+print("Inferencing with CPU took {:.2f}ms".format((cpuEndTime-cpuStartTime)*1000.0))
+
+
+
+
+# Turn on GPU mode and use device zero for training
+caffe.set_mode_gpu()
+caffe.set_device(0)
+
+gpuStartTime = time.time()
+
+### perform classification
+output = net.forward()
+
+gpuEndTime = time.time()
+
+print("Inferencing with GPU took {:.2f}ms".format((gpuEndTime-gpuStartTime)*1000.0))
+
 output_prob = output['loss'][0]  # the output probability vector for the first image in the batch
 
-#Add the correct digits to correspond with the output probablility,
+#Specify labels to correspond with the output probablility,
 #in many casees you will have text classifications e.g. "dogs", "birds", etc.
-digits_label = [0,1,2,3,4,5,6,7,8,9]
+digits_label = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
 
 #Find the index with the highest probablility
 highest_index = -1
 highest_probability = 0.0
 
-for i in range(0,9):
+for i in range(10):
     if output_prob[i] > highest_probability:
         highest_index = i
         highest_probability = output_prob[i]
@@ -49,4 +73,4 @@ for i in range(0,9):
 if highest_index < 0:
     print("Did not detect a number!")
 else:
-    print("Digit "+ str(digits_label[highest_index]) + " detected with " + str(highest_probability*100.0)+"%  probability.")
+    print("Digit {:s} detected with {:.2f}%  probability.".format(digits_label[highest_index], highest_probability*100.0) )

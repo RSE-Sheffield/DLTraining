64d65529ad41358dd5de3011cb733f0d0b0dfa96
Twin Karmakharm <hi@twin.uk.com>
2017-02-23 00:23:36 +0000

    Fixed codes in lab 1-3, added lab 4-6

--- code/lab06/train_text_gen.sh
+++ code/lab06/train_text_gen.sh
@@ -5,4 +5,4 @@ module load libs/caffe/rc3/gcc-4.9.4-cuda-8.0-cudnn-5.1-conda-3.4-TESTING
 source activate caffe
 export LD_LIBRARY_PATH="/home/$USER/.conda/envs/caffe/lib:$LD_LIBRARY_PATH"
 
-caffe train -solver=code/lab02/mnist_lenet_solver.prototxt
+caffe train -solver=code/lab06/text_gen_solver.prototxt

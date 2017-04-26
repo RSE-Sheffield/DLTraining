12f9a2248d5cd9d28eadaf4a8197f1681283d66c
Twin <hi@twin.uk.com>
2017-04-26 17:40:40 +0100

    Updated code to new Caffe module

--- code/lab05/benchmark_mnist.sh
+++ code/lab05/benchmark_mnist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #$ -l gpu=2 -P rse-training -q rse-training.q -l rmem=10G -j y
 
-module load libs/caffe/rc3/gcc-4.9.4-cuda-8.0-cudnn-5.1-conda-3.4-TESTING
-source activate caffe
-export LD_LIBRARY_PATH="/home/$USER/.conda/envs/caffe/lib:$LD_LIBRARY_PATH"
+module load apps/caffe/rc5/gcc-4.9.4-cuda-8.0-cudnn-5.1
+
+
 
 # (These example calls require you complete the LeNet / MNIST example first.)
 # time LeNet training on CPU for 10 iterations
